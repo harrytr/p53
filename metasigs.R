710fdf361dbc8e9223af78090af7c76636e68171
Harry Triantafyllidis <harrytr@users.noreply.github.com>
2022-06-25 10:06:57 +0100

    Add files via upload

--- metasigs.R
+++ metasigs.R
@@ -12,20 +12,12 @@ metasigs <- function(database) {
     df_primal <- as.data.frame(read.csv(files[i]), header = TRUE)
     
     sig <- as.list(unique(df_primal[2]))
-
-    #write.csv(sig,paste0("unique_",files[i]))
-
     all_sigs[i] <- as.list(sig)
     names(all_sigs)[i] <- paste0(database,"_",files[i])
-    #names(all_sigs[i]) <- files[i]
-    
-    
-    #test_bed <- list(regulon_Dorothea = as.list(unique(as.character(regulons$target_genesymbol))), GLM_sig = as.list(GLM_sig),
-    #                 CELL_REPORTS_TP53_sig = tp53_sig, MAPK_pathway_genes = as.list(V(GRN)$name) )
+
   }
 
   upset(fromList(all_sigs),order.by = "freq")
-  #plot(g)
   
   
 }
