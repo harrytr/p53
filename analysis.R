aa0f957b4d1baf7b53a620b5c27aca9702ef059c
Harry Triantafyllidis <harrytr@users.noreply.github.com>
2022-09-20 15:21:28 +0100

    Add files via upload
    Latest Renoir package supported 09/2022

--- analysis.R
+++ analysis.R
@@ -2,51 +2,111 @@
 MNR <-function(regression_data,perm,disease_filename, reg_type, resDir, screening_method, key)  {
   # install.packages(c("glmnet","Matrix","parallel","doParallel","foreach","stats","utils","matrixTests","graphics"))
   #  install.packages(c("testthat","knitr","rmarkdown","plotly","htmltools","kableExtra","DT"))
-  #  setwd("C:/Users/Harry/Documents/OneDrive - Nexus365/Desktop/shinyapp/results")
-  #Load the library
+
   library(renoir)
 
+  
+  get_hp = function(id, y){
+    
+    #Generalised Linear Models with Penalisation
+    lambda = 10^seq(3, -2, length=100)
+    # alpha = seq(0.1, 0.9, length = 9)
+    alpha = seq(0.1, 0.9, length = 5)
+    gamma = c(0, 0.25, 0.5, 0.75, 1)
+    
+    #Random Forest
+    ntree = c(10, 50, 100, 250, 500)
+    
+    #Generalised Boosted Regression Modelling
+    eta = c(0.3, 0.1, 0.01, 0.001)
+    
+    #Support Vector Machines
+    cost      = 2^seq(from = -5, to = 15, length.out = 5)
+    svm.gamma = 2^seq(from = -15, to = 3, length.out = 4)
+    degree    = seq(from = 1, to = 3, length.out = 3)
+    #Note that for classification nu must be
+    #nu * length(y)/2 <= min(table(y)). So instead of
+    #fixing it as
+    # nu        = seq(from = 0.1, to = 0.6, length.out = 5)
+    #we do
+    nu.to = floor((min(table(y)) * 2/length(y)) * 10) / 10
+    nu = seq(from = 0.1, to = nu.to, length.out = 5)
+    
+    #kNN
+    k = seq(from = 1, to = 9, length.out = 5)
+    
+    #Nearest Shrunken Centroid
+    threshold = seq(0, 2, length.out = 30)
+    
+    #hyperparameters
+    out = switch(
+      id,
+      'lasso'              = list(lambda = lambda),
+      'ridge'              = list(lambda = lambda),
+      'elasticnet'         = list(lambda = lambda, alpha = alpha),
+      'relaxed_lasso'      = list(lambda = lambda, gamma = gamma),
+      'relaxed_ridge'      = list(lambda = lambda, gamma = gamma),
+      'relaxed_elasticnet' = list(lambda = lambda, gamma = gamma, alpha = alpha),
+      'randomForest'       = list(ntree = ntree),
+      'gbm'                = list(eta = eta, ntree = ntree),
+      'linear_SVM'         = list(cost = cost),
+      'polynomial_SVM'     = list(cost = cost, gamma = svm.gamma, degree = degree),
+      'radial_SVM'         = list(cost = cost, gamma = svm.gamma),
+      'sigmoid_SVM'        = list(cost = cost, gamma = svm.gamma),
+      'linear_NuSVM'       = list(nu = nu),
+      'polynomial_NuSVM'   = list(nu = nu, gamma = svm.gamma, degree = degree),
+      'radial_NuSVM'       = list(nu = nu, gamma = svm.gamma),
+      'sigmoid_NuSVM'      = list(nu = nu, gamma = svm.gamma),
+      'gknn'               = list(k = k),
+      'nsc'                = list(threshold = threshold)
+    )
+    
+    return(out)
+  }
   #Set some directories
   #Input file must be in the directory defined here
   #for the following code to be run without changes
-  
+
   data = readRDS(regression_data)
-  
+
   colnames(data) <- sapply(strsplit(colnames(data), split='..', fixed=TRUE),function(x) (x[1]))
-  
+
   analysis.type = reg_type
   #-------------------------------------------------------------------------------#
   #Set X
-  
+
   #Shape data as needed
   dataX = as.matrix(x = data[,-(1),drop=F])
-  
-  
+
+
   #test is numeric
-  is.numeric(dataX)
+  
+  print(is.numeric(dataX))
+
   #attach row names
   rownames(dataX) = data$Variant_Classification
-  
+
   #Check NAs
   #test = sum(colSums(is.na(dataX)))
-  
+
   #-------------------------------------------------------------------------------#
   #Set Y
-  
+
   #Shape data as needed
-  
+
   dataY = as.factor(data[,1])
   table(dataY)
-  
+
   # dataY = droplevels(data[,3,drop=T])
-  
-  
+
+
   #-------------------------------------------------#
   if(identical(analysis.type, "binomial")){
     #BINOMIAL RESPONSE TYPE
     #Response type
+    print("Binomial classification starting...")
     resp.type = "binomial"
-    
+
     #Check which is Missense Mutation
     # index = which(dataY == "Missense_Mutation")
     #Create the Y
@@ -56,38 +116,37 @@ MNR <-function(regression_data,perm,disease_filename, reg_type, resDir, screenin
 
     #attach row names
     names(dataY) = data$Variant_Classification
-    
+
     #Update x
     dataX = dataX[names(dataY),,drop=F]
-    
-    
+
   } else if(identical(analysis.type, "multinomial")){
     #Response type
     resp.type = "multinomial"
-    
+
     #Check data
     #table(dataY)
-    
+
     #Remove In_Frame_Ins (samples numbers too low)
     #
-    
+
     data.sub = data[data$Variant_Classification != "In_Frame_Ins",,drop=F]
-    
-    
+
+
     #Shape data as needed
-    
+
     dataY = droplevels(as.factor(data.sub[,3,drop=T]))
-    
+
     #Check new data
     table(dataY)
-    
+
     #attach row names
     names(dataY) = data.sub$CELLLINE
-    
+
     dataX = dataX[names(dataY),,drop=F]
-    
+
   }
-  
+
   get_typemeasure = function(resp.type){
     type.measure = switch(EXPR = resp.type,
                           # gaussian    = c("mse", "deviance", "mae"),
@@ -95,102 +154,180 @@ MNR <-function(regression_data,perm,disease_filename, reg_type, resDir, screenin
                           binomial    = c("mse", "deviance", "class", "auc", "mae"),
                           multinomial = c("mse", "deviance", "class", "mae"));
     return(type.measure);
-    
+
   }
   #as factor
   dataY = as.factor(dataY)
+
+
+
   #-------------------------------------------------------------------------------#
-  #Train And Test
-  
-  #Some parameters to set
-  #a) number of repeats: should be set >= 10.
-  # Try some numbers. I would use 100, but if too slow you should decrease it.
-  n = perm
-  #b) number of grid points: should be >=3.
-  # 3 is already ok, if execution is fast on your pc you could use 5.
-  n.grid.points = 3;
-  
-  #Write your output directory
+  # NEW SET UP
+  #-------------------------------------------------------------------------------#
+  #learning method
+  learning.method.id = "elasticnet"
+
+  #sampling for tuning
+  sampling.method.id.tuning = "cv"
+
+  #sampling for evaluation
+  sampling.method.id.evaluation = "random"
+
+  #metric for tuning
+  performance.metric.id.tuning = "acc" #MUST BE ONE METRIC
+
+  #metrics for evaluation
+  performance.metric.ids.evaluation = c("acc", "precision") #YOU CAN WRITE HERE MORE THAN ONE METRIC
+
+  #-------------------------------------------------------------------------------#
+  #Filter
+  filterL = FilterList(
+    Filter(id = "na"),
+    Filter(id = "intensity"),
+    Filter(id = "variability")
+  )
+
+  #-------------------------------------------------------------------------------#
+  #Screeners
+  screeners = ScreenerList(
+    Screener(id = "ebayes", parameters = list(assay.type = "array", logged2 = T)),
+    Screener(id = "permutation", parameters = list(assay.type = "array", logged2 = T))
+  )
+
+  #-------------------------------------------------------------------------------#
+  #tuner
+
+  tuner = Tuner(
+    id = "grid.search",
+    sampler = Sampler(
+      method = sampling.method.id.tuning,
+      k = 10L,
+      n = integer(),
+      strata = dataY
+    ),
+    screener = screeners,
+    looper   = Looper(cores = 1L),
+    logger   = Logger(verbose = T, level = "INFO")
+  )
+
+
+  #-------------------------------------------------------------------------------#
+  #Learner
+  learner = Learner(
+    tuner      = tuner,
+    trainer    = Trainer(id = learning.method.id),
+    forecaster = Forecaster(id = learning.method.id),
+    scorer     = ScorerList(Scorer(id = performance.metric.id.tuning)),
+    selector   = Selector(id = learning.method.id),
+    recorder   = Recorder(id = learning.method.id),
+    marker     = Marker(id = learning.method.id),
+    logger     = Logger(level = "ALL")
+  )
+
+
+  #-------------------------------------------------------------------------------#
+  #ScorerList
+  sl = list()
+  for(i in performance.metric.ids.evaluation){
+    sl[[i]] = Scorer(id = i)
+  }
+  sl = ScorerList(sl)
+
+  #Evaluator
+
+    
+    evaluator = Evaluator(
+      #Sampling strategy: stratified random sampling without replacement
+      sampler = Sampler(               
+        method = "random",             
+        k = 10L,                       
+        strata = dataY,                    
+        N = as.integer(length(dataY))      
+      ),
+      
+    #Performance metric
+    #Each chosen metric must be in a different Scorer, so if you select 3 metrics you will have to add another Scorer in the ScorerList
+    scorer  = sl,
+
+    #looper  = Looper(cores = 1L)#sequential Loop
+    looper  = Looper(cores = 1000L)#parallel Loop
+  )
+
+  #-------------------------------------------------------------------------------#
+  # Train And Test
+  #-------------------------------------------------------------------------------#
+
   saveRDS(dataY, file = "dataY.rds")
   print("Running renoir...")
-  res = renoir::run(
-    x = dataX,
-    y = dataY,
-    resp.type = resp.type,
-    
-    #Pre-processing
-    filter = TRUE,
-    threshold.na = 0.5,
-    threshold.variability = 1,
-    na.omit = FALSE,
-    
-    #Training And Testing
-    resampling.method = "multi.random",
-    
-    n = n,
-    min.obs = 10 ,#must be equal to nfolds for glmnet
-    n.grid.points = n.grid.points,
-    balance = TRUE,
-    # balance = FALSE, #you need the bug free version of glmnet to run this option
-    do.parallel = TRUE,
-    num.cores = 1000,
-    restore = FALSE,
-    learning.method = "glmnet",
-    #Screening
-    screening.method = screening_method,
-    screening.args = list(sam.args = list(assay.type = "array", logged2 = T),
-                          limma.args = list(assay.type = "array", logged2 = T)),
-    
-    #screening.nvar = seq(50, length(colnames(data)) , round(length(colnames(data))/4)),
-    screening.nvar=c(50, 100, 150, 238),
-    #Confidence intervals
-    confidence = 0.95,
-    importance.term = "coefficient",
-    random.seed = NULL,
-    #Log
-    log.path = file.path(resDir, "log.txt"),
-    verbose = TRUE,
-    
-    #Outdir
-    outdir = resDir,
-    filename = NULL,
-    
-    #Further arguments
-    keep.call = FALSE,
-    keep.fit = F,
-    relax = FALSE);
-  
-  #Extract the best model
-  #a) Select the configuration to use
-  best.config = c("min", "1se")[1]
-  # 
-  # #b) Select the measure type
-  type.measure = c("mse", "deviance", "class", "auc", "mae")[3]
-  # #c) Plot the results
-  # plot(object = renoir, best.config = best.config, type.measure = type.measure, nc = length(levels(dataY)))
-  # 
-  # 
-  # #d) Select the best model
-  #best_model = bm(object = res, best.config = best.config, type.measure = type.measure)
-  saveRDS(res,paste0(resDir,"\\best_model.rds"))
-  #invisible(readline(prompt="Press [enter] to continue"))
+  res <- renoir(
+    filter = filterL,
 
-   # #e) Probability of recruitment for each feature
-  # View(best_model$precruit)
-  renoir::save_plots_2xpage(res, type.measure = c("mse", "deviance", "class", "mae"),  outdir = resDir, filename = disease_filename)
+    #Number of considered training set sizes
+    npoints = 3,
+    # ngrid,
+    ##Minimum number of samples in the grid
+    nmin = round(nrow(dataX)/2),
+    #nmin = 5,
+
+    #Loop
+    looper = Looper(),
+
+    #Store
+    filename = "renoir",
+    outdir   = resDir,
+    restore  = TRUE,
+
+    #Learn
+    learner   = learner,
+
+    #Evaluate
+    evaluator = evaluator,
+
+    #Log
+    logger    = Logger(path = file.path(resDir, "log.txt"), level = "ALL", verbose = T),
+
+    #Data for training
+    hyperparameters = get_hp(id = learning.method.id, y = y),
+    x         = dataX,
+    y         = dataY,
+    weights   = NULL,
+    offset    = NULL,
+    resp.type = resp.type,
+
+    #Free space
+    rm.call = FALSE,
+    rm.fit  = FALSE,
+
+    #Group results
+    grouping = TRUE,
+
+    #Screening
+    screening = NULL,
+
+    #Remove call from trainer to reduce space
+    keep.call = F
+  )
+
+  saveRDS(res,paste0(resDir,"/best_model.rds"))
   
-  #YOU NEED TO SET THIS UP WITH YOUR VALUES
-  # converison.table must be a dataframe with rows as in dataX columns, or with one column 
-  # with the features. The other columns can be annotation (e.g. features is entrez id, another column is hgcn symbol)
-  
-  #feat.index = the index of the columns of conversion.table that contains the features
-  
-  renoir::create_report(object = res,
-                        outdir = resDir, 
+  renoir:::create_report(object = res[[1]],
+                        outdir = resDir,
                         filename = paste0('report_multir_',disease_filename, "_", resp.type,'_CCLE'),
-                        annotation = NULL, 
+                        report.type = "full",
+                        annotation = NULL,
                         feat.index = 1,
                         tabset = TRUE)
-  
-  
+
+  # renoir:::plot.RenoirSummaryTable(
+  #   x = res[res$config == "opt",,drop=F],
+  #   measure     = "precision", 
+  #   set         = "full", 
+  #   interactive = T, 
+  #   add.boxplot = F,
+  #   add.scores  = F,
+  #   add.best    = F,
+  #   key         = c("id", "config")
+  # )
+  # 
+
 }
